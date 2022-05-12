bb95e3651aee5d07c1a6ac920e587cb375777da0
rivques <38469076+rivques@users.noreply.github.com>
2022-05-12 11:04:52 -0400

    BooststealerReward tries to go fast and get boost

--- booststealer_reward.py
+++ booststealer_reward.py
@@ -5,14 +5,17 @@ import numpy as np
 
 class BooststealerReward(RewardFunction):
     def reset(self, initial_state: GameState):
-        self.old_boost = None
+        self.old_boost = 0
     
     def get_reward(self, player: PlayerData, state: GameState, previous_action: np.ndarray) -> float:
         linear_velocity = player.car_data.linear_velocity
-        reward = math.vecmag(linear_velocity) / 2300 # linear scale reward from 0 to 1 for supersonic
-        
-        reward = 0
-        
+        speed_reward = math.vecmag(linear_velocity) / 2200 # linear scale reward from 0 to 1 for supersonic
+        reward += 3 if speed_reward > 1 else speed_reward # add a bonus for supersonic
+
+        net_boost_change = player.car_data.boost - self.old_boost
+        reward += net_boost_change if net_boost_change > 0 else 0 # add a bonus for picking up boost
+
+        self.old_boost = player.car_data.boost
         return reward
     
     def get_final_reward(self, player: PlayerData, state: GameState, previous_action: np.ndarray) -> float:
